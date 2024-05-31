084ca6c0f4e17764694570d8063f5a16081134d8
vargaae <vargaae@hotmail.com>
2024-05-31 20:42:04 +0200

    1-02-14-Posts Service Creation+Testing

--- posts/index.js
+++ posts/index.js
@@ -1,15 +1,32 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
 
 const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
 
 const posts = {};
+
+app.get("/", (req, res) => {
+  res.send(`Posts service is Working on port ${port}`);
+});
 
 app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {});
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
 
-app.listen(4000, () => {
-  console.log("Listening q on 4000");
+  posts[id] = {
+    id,
+    title,
+  };
+});
+
+app.listen(port, () => {
+  console.log(`Listening q on port ${port}`);
 });
