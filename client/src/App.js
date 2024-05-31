f97a2fa6e20612222bcf7c05c0aa46e40bb52466
vargaae <vargaae@hotmail.com>
2024-05-31 21:52:12 +0200

    1-02-19-React Project Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Blog App <code>Posts - </code> Comments
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-link" href="">
+          Blog App
         </a>
       </header>
     </div>
